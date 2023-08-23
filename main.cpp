d942c856418d6addbcd0f1f5eb07619f68bbfc2e
Danylo Lizanets <danylo.lizanets@nokia.com>
2023-08-23 04:28:42 +0200

    Fixes.

--- main.cpp
+++ main.cpp
@@ -35,6 +35,7 @@ struct Enemy {
   float y = 0;
   float speed = 4;
   int hp = 10;
+  bool is_boss = false;
   bool alive = false;
 };
 
@@ -50,6 +51,7 @@ struct Background {
   float x;
   float y;
   float size;
+  float speed;
   int color = 128;
   bool alive = false;
 };
@@ -90,15 +92,24 @@ void MoveBullet(Bullet &b) {
   }
 }
 void DrawEnemy(Enemy e) {
-  DrawPoly(Vector2{e.x, e.y - e.speed}, 4, std::max(16, e.hp), 45,
-           Color{255, 0, 0, 128});
-  DrawPoly(Vector2{e.x, e.y}, 4, std::max(16, e.hp), 45, RED);
+  auto hp = std::min(300, std::max(16, e.hp));
+  DrawRectangle(e.x - hp - (e.is_boss ? e.speed : 0), e.y - hp - (e.is_boss ? 0 : e.speed), hp * 2, hp * 2,
+                Color{255, 0, 0, 128});
+  DrawRectangle(e.x - hp, e.y - hp, hp * 2, hp * 2, e.is_boss ? MAROON : RED);
 }
 void MoveEnemy(Enemy &e) {
   auto w = GetScreenWidth();
   auto h = GetScreenHeight();
-  e.y += e.speed;
-  if (e.x < 0 || e.y < 0 || e.x > w || e.y > h) {
+  if (e.is_boss) {
+    e.x += e.speed;
+    if (e.x < 0 || e.x > w) {
+      e.speed *= -1;
+      e.x += 2 * e.speed;
+    }
+  } else {
+    e.y += e.speed;
+  }
+  if (e.x < 0 || e.y < -e.hp || e.x > w || e.y > h + e.hp) {
     e.alive = false;
   }
 }
@@ -122,13 +133,15 @@ void DrawBackground(Background b) {
 void MoveBackground(Background &b) {
   auto w = GetScreenWidth();
   auto h = GetScreenHeight();
-  b.y += 1;
+  b.y += b.speed;
   if (b.x < 0 || b.y < -b.size || b.x > w || b.y > h + b.size) {
     b.alive = false;
   }
 }
 
 int main(int argc, char *argv[]) {
+  SetRandomSeed(time(NULL));
+
   auto win_w = 1000;
   auto win_h = 1000;
 
@@ -151,10 +164,12 @@ int main(int argc, char *argv[]) {
 
   bool game_over = false;
   bool pause = true;
+  bool boss_is_here = false;
   int score = 0;
   int stage = 1;
 
   while (!WindowShouldClose()) {
+    BeginDrawing();
     if (!game_over && !pause) {
       if (frame_counter % 6 == 0) {
         for (int i = 0; i < p.cannons; ++i) {
@@ -177,12 +192,23 @@ int main(int argc, char *argv[]) {
       size_t difficulty = frame_counter / 360;
       size_t frequency = std::max(1ul, 30 - difficulty - stage);
       if (frame_counter % frequency == 0) {
-        for (int i = 0; i < stage; ++i) {
+        for (int i = 0; i < 1; ++i) {
           enemies[dead_enemy].alive = true;
           enemies[dead_enemy].x = GetRandomValue(0, GetScreenWidth());
-          enemies[dead_enemy].y = 0;
-          enemies[dead_enemy].speed = 4 + (2 - log10(GetRandomValue(1, 1000)));
-          enemies[dead_enemy].hp = GetRandomValue(10, 100) + difficulty * stage;
+          if (!boss_is_here && stage % 3 == 0) {
+            boss_is_here = true;
+            enemies[dead_enemy].speed = 5;
+            enemies[dead_enemy].hp = 100000;
+            enemies[dead_enemy].is_boss = true;
+            enemies[dead_enemy].y = 0;
+          } else {
+            enemies[dead_enemy].speed =
+                4 + (2 - log10(GetRandomValue(1, 1000)));
+            enemies[dead_enemy].hp =
+                GetRandomValue(10, 70) + difficulty * stage;
+            enemies[dead_enemy].is_boss = false;
+            enemies[dead_enemy].y = -enemies[dead_enemy].hp;
+          }
 
           while (true) {
             dead_enemy = (dead_enemy + 1) % ENEMIES;
@@ -207,13 +233,14 @@ int main(int argc, char *argv[]) {
         }
       }
 
-      if (frame_counter % 60 == 0) {
-        for (int i = 0; i < 1; ++i) {
+      if (frame_counter % 120 == 0) {
+        for (int i = 0; i < 2; ++i) {
           backgrounds[dead_background].alive = true;
           backgrounds[dead_background].size = GetRandomValue(60, 600);
           backgrounds[dead_background].x = GetRandomValue(0, GetScreenWidth());
           backgrounds[dead_background].y = -backgrounds[dead_background].size;
           backgrounds[dead_background].color = 128 + GetRandomValue(-64, 64);
+          backgrounds[dead_background].speed = i ? 1 : 0.5;
 
           while (true) {
             dead_background = (dead_background + 1) % BACKGROUNDS;
@@ -233,44 +260,7 @@ int main(int argc, char *argv[]) {
           rocket.dy = -5;
         }
       }
-    }
 
-    auto old_frame_counter = frame_counter;
-    frame_counter = (frame_counter + 1) % (FPS * 60);
-    if (!game_over && !pause && old_frame_counter > frame_counter) {
-      stage += 1;
-    }
-
-    if (!game_over && IsKeyPressed(KEY_SPACE)) {
-      pause = !pause;
-      if (pause) {
-        frame_counter_backup = frame_counter;
-      } else {
-        frame_counter = frame_counter_backup;
-      }
-    }
-    if (game_over && IsKeyPressed(KEY_SPACE)) {
-      score = 0;
-      stage = 1;
-      for (auto &background : backgrounds) {
-        background.alive = false;
-      }
-      for (auto &bullet : bullets) {
-        bullet.alive = false;
-      }
-      for (auto &enemy : enemies) {
-        enemy.alive = false;
-      }
-      for (auto &bonus : bonuses) {
-        bonus.alive = false;
-      }
-      p = Player{float(win_w / 2), float(win_h - 40), 5, 1, 10, 1};
-      rocket.alive = false;
-      game_over = false;
-      pause = true;
-      frame_counter = 0;
-    }
-    if (!game_over && !pause) {
       MovePlayer(p);
       for (auto &background : backgrounds) {
         if (background.alive) {
@@ -282,89 +272,14 @@ int main(int argc, char *argv[]) {
           MoveBullet(bullet);
         }
       }
-      float min_d = std::numeric_limits<float>::max();
-      int counter = -1;
-      int min_idx = 0;
-      for (auto &enemy : enemies) {
-        counter += 1;
-        if (enemy.alive) {
-          MoveEnemy(enemy);
-          if (rocket.alive &&
-              CheckCollisionRecs(
-                  Rectangle{rocket.x - 10, rocket.y - 10, 20, 20},
-                  Rectangle{enemy.x - 10, enemy.y - 10, 20, 20})) {
-            enemy.alive = false;
-            rocket.alive = false;
-            continue;
-          }
-          if (rocket.alive) {
-            auto d = std::sqrt(std::pow(rocket.x - enemy.x, 2.0f) +
-                               std::pow(rocket.y - enemy.y, 2.0f));
-            if (d < min_d) {
-              min_d = d;
-              min_idx = counter;
-            }
-          }
-          auto hp = std::max(16, enemy.hp);
-          if (CheckCollisionCircleRec(Vector2{p.x, p.y}, 12,
-                                      Rectangle{enemy.x - hp, enemy.y - hp,
-                                                hp * 2.0f, hp * 2.0f})) {
-            game_over = true;
-          }
-        }
-      }
-      if (rocket.alive) {
-        auto dx = enemies[min_idx].x - rocket.x;
-        auto dy = enemies[min_idx].y - rocket.y;
-        rocket.dx = 7 * dx / min_d;
-        rocket.dy = 7 * dy / min_d;
-      }
       for (auto &bonus : bonuses) {
         if (bonus.alive) {
           MoveBonus(bonus);
-          if (CheckCollisionRecs(
-                  Rectangle{p.x - 15, p.y - 15, 30, 30},
-                  Rectangle{bonus.x - 10, bonus.y - 10, 20, 20})) {
-            switch (bonus.typ) {
-            case 1:
-              p.speed += 1;
-              break;
-            case 2:
-              p.cannons += 1;
-              break;
-            case 3:
-              p.bullet_damage += 1;
-              break;
-            case 4:
-              p.bullet_penetration += 1;
-              break;
-            default:
-              break;
-            }
-            bonus.alive = false;
-          }
         }
       }
-      for (auto &bullet : bullets) {
-        if (bullet.alive) {
-          for (auto &enemy : enemies) {
-            if (enemy.alive) {
-              auto hp = std::max(16, enemy.hp);
-              if (CheckCollisionCircleRec(Vector2{bullet.x, bullet.y}, 2,
-                                          Rectangle{enemy.x - hp, enemy.y - hp,
-                                                    hp * 2.0f, hp * 2.0f})) {
-                enemy.hp -= p.bullet_damage;
-                bullet.penetration -= 1;
-                if (bullet.penetration == 0) {
-                  bullet.alive = false;
-                }
-                if (enemy.hp <= 0) {
-                  enemy.alive = false;
-                  score += 1;
-                }
-              }
-            }
-          }
+      for (auto &enemy : enemies) {
+        if (enemy.alive) {
+          MoveEnemy(enemy);
         }
       }
       if (rocket.alive) {
@@ -377,21 +292,20 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    BeginDrawing();
     ClearBackground(BLACK);
     for (auto &background : backgrounds) {
       if (background.alive) {
         DrawBackground(background);
       }
     }
-    for (auto &bullet : bullets) {
-      if (bullet.alive) {
-        DrawBullet(bullet);
-      }
-    }
     for (auto &enemy : enemies) {
       if (enemy.alive) {
         DrawEnemy(enemy);
+      }
+    }
+    for (auto &bullet : bullets) {
+      if (bullet.alive) {
+        DrawBullet(bullet);
       }
     }
     for (auto &bonus : bonuses) {
@@ -423,6 +337,132 @@ int main(int argc, char *argv[]) {
                          p.bullet_penetration, p.cannons)
                  .c_str(),
              10, 10, 20, RAYWHITE);
+
+    if (!game_over && IsKeyPressed(KEY_SPACE)) {
+      pause = !pause;
+      if (pause) {
+        frame_counter_backup = frame_counter;
+      } else {
+        frame_counter = frame_counter_backup;
+      }
+    }
+    if (game_over && IsKeyPressed(KEY_SPACE)) {
+      score = 0;
+      stage = 1;
+      boss_is_here = false;
+      for (auto &background : backgrounds) {
+        background.alive = false;
+      }
+      for (auto &bullet : bullets) {
+        bullet.alive = false;
+      }
+      for (auto &enemy : enemies) {
+        enemy.alive = false;
+      }
+      for (auto &bonus : bonuses) {
+        bonus.alive = false;
+      }
+      p = Player{float(win_w / 2), float(win_h - 40), 5, 1, 10, 1};
+      rocket.alive = false;
+      game_over = false;
+      pause = true;
+      frame_counter = 0;
+    }
+    if (!game_over && !pause) {
+      float min_d = std::numeric_limits<float>::max();
+      int counter = -1;
+      int min_idx = 0;
+      for (auto &enemy : enemies) {
+        counter += 1;
+        if (enemy.alive) {
+          if (rocket.alive &&
+              CheckCollisionRecs(
+                  Rectangle{rocket.x - 10, rocket.y - 10, 20, 20},
+                  Rectangle{enemy.x - 10, enemy.y - 10, 20, 20})) {
+            enemy.alive = false;
+            rocket.alive = false;
+            continue;
+          }
+          if (rocket.alive) {
+            auto d = std::sqrt(std::pow(rocket.x - enemy.x, 2.0f) +
+                               std::pow(rocket.y - enemy.y, 2.0f));
+            if (d < min_d) {
+              min_d = d;
+              min_idx = counter;
+            }
+          }
+          auto hp = std::min(300, std::max(16, enemy.hp));
+          if (CheckCollisionCircleRec(Vector2{p.x, p.y}, 12,
+                                      Rectangle{enemy.x - hp, enemy.y - hp,
+                                                hp * 2.0f, hp * 2.0f})) {
+            game_over = true;
+          }
+        }
+      }
+      if (rocket.alive) {
+        auto dx = enemies[min_idx].x - rocket.x;
+        auto dy = enemies[min_idx].y - rocket.y;
+        rocket.dx = 7 * dx / min_d;
+        rocket.dy = 7 * dy / min_d;
+      }
+      for (auto &bonus : bonuses) {
+        if (bonus.alive) {
+          if (CheckCollisionRecs(
+                  Rectangle{p.x - 15, p.y - 15, 30, 30},
+                  Rectangle{bonus.x - 10, bonus.y - 10, 20, 20})) {
+            switch (bonus.typ) {
+            case 1:
+              p.speed += 1;
+              break;
+            case 2:
+              p.cannons += 1;
+              break;
+            case 3:
+              p.bullet_damage += 1;
+              break;
+            case 4:
+              p.bullet_penetration += 1;
+              break;
+            default:
+              break;
+            }
+            bonus.alive = false;
+          }
+        }
+      }
+      for (auto &bullet : bullets) {
+        if (bullet.alive) {
+          for (auto &enemy : enemies) {
+            if (enemy.alive) {
+              auto hp = std::min(300, std::max(16, enemy.hp));
+              auto rect =
+                  Rectangle{enemy.x - hp, enemy.y - hp, hp * 2.0f, hp * 2.0f};
+              if (CheckCollisionPointRec(Vector2{bullet.x, bullet.y}, rect)) {
+                enemy.hp -= p.bullet_damage;
+                bullet.penetration -= 1;
+                if (bullet.penetration == 0) {
+                  bullet.alive = false;
+                }
+                if (enemy.hp <= 0) {
+                  enemy.alive = false;
+                  if (enemy.is_boss) {
+                    score += 500;
+                  }
+                  score += 1;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    auto old_frame_counter = frame_counter;
+    frame_counter = (frame_counter + 1) % (FPS * 60);
+    if (!game_over && !pause && old_frame_counter > frame_counter) {
+      stage += 1;
+      boss_is_here = false;
+    }
     EndDrawing();
   }
 
